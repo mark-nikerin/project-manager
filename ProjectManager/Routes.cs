d5d078aface5b5524068d17ac2ba55f944d0be25
Mark Nikerin <nikerin@mercurydevelopment.com>
2021-05-12 21:45:05 +0400

    Add routing

--- ProjectManager/Routes.cs
+++ ProjectManager/Routes.cs
@@ -9,6 +9,85 @@
             public const string GetProject = "api/projects/{id:int}";
             public const string DeleteProject = "api/projects/{id:int}";
             public const string UpdateProject = "api/projects/{id:int}";
+
+            public static class Tasks
+            {
+                public static class Tags
+                {
+                    public const string GetTags = "api/projects/{id:int}/tasks/tags";
+                    public const string AddTag = "api/projects/{id:int}/tasks/tags";
+                    public const string GetTag = "api/projects/{projectId:int}/tasks/tags/{id:int}";
+                    public const string DeleteTag = "api/projects/{projectId:int}/tasks/tags/{id:int}";
+                    public const string UpdateTag = "api/projects/{projectId:int}/tasks/tags/{id:int}";
+                }
+
+                public static class Types
+                {
+                    public const string GetTags = "api/projects/{id:int}/tasks/types";
+                    public const string AddTag = "api/projects/{id:int}/tasks/types";
+                    public const string GetTag = "api/projects/{projectId:int}/tasks/types/{id:int}";
+                    public const string DeleteTag = "api/projects/{projectId:int}/tasks/types/{id:int}";
+                    public const string UpdateTag = "api/projects/{projectId:int}/tasks/types/{id:int}";
+                }
+
+                public static class Statuses
+                {
+                    public const string GetTags = "api/projects/{id:int}/tasks/statuses";
+                    public const string AddTag = "api/projects/{id:int}/tasks/statuses";
+                    public const string GetTag = "api/projects/{projectId:int}/tasks/statuses/{id:int}";
+                    public const string DeleteTag = "api/projects/{projectId:int}/tasks/statuses/{id:int}";
+                    public const string UpdateTag = "api/projects/{projectId:int}/tasks/statuses/{id:int}";
+                }
+            }
+        }
+
+        public static class Boards
+        {
+            public const string GetBoards = "api/boards";
+            public const string AddBoard = "api/boards";
+            public const string GetBoard = "api/boards/{id:int}";
+            public const string DeleteBoard = "api/boards/{id:int}";
+            public const string UpdateBoard = "api/boards/{id:int}";
+        }
+
+        public static class Iterations
+        {
+            public const string GetIterations = "api/iterations";
+            public const string AddIteration = "api/iterations";
+            public const string GetIteration = "api/iterations/{id:int}";
+            public const string DeleteIteration = "api/iterations/{id:int}";
+            public const string UpdateIteration = "api/iterations/{id:int}";
+        }
+
+        public static class Tasks
+        {
+            public const string GetTasks = "api/tasks";
+            public const string AddTask = "api/tasks";
+            public const string GetTask = "api/tasks/{id:int}";
+            public const string DeleteTask = "api/tasks/{id:int}";
+            public const string UpdateTask = "api/tasks/{id:int}";
+
+            public static class Comments
+            {
+                public const string GetComments = "api/tasks/{taskId:int}/comments";
+                public const string AddComment = "api/tasks/{taskId:int}/comments";
+                public const string DeleteComment = "api/tasks/{taskId:int}/comments/{id:int}";
+                public const string UpdateComment = "api/tasks/{taskId:int}/comments/{id:int}";
+            }
+        }
+
+        public static class WorkTimeRecords
+        {
+            public const string GetRecords = "api/work/records";
+            public const string AddRecord = "api/work/records";
+            public const string DeleteRecord = "api/work/records/{id:int}";
+            public const string UpdateRecord = "api/work/records/{id:int}";
+        }
+
+        public static class Auth
+        {
+            public const string Register = "api/auth/register";
+            public const string Login = "api/auth/login";
         }
     }
 }
